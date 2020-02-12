dd775de8bbd1901c1a873b412248723c02195bc6
Jannis <jannis@pixel-pink.de>
2020-02-12 17:53:23 +0100

    added styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-stripe`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
