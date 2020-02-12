14dd122369648eb0f7cead4f9050d6b73875541f
Jannis <jannis@pixel-pink.de>
2020-02-12 11:34:07 +0100

    stripe integration

---
+++ src/components/checkout.js
@@ -0,0 +1,53 @@
+import React from "react"
+const buttonStyles = {
+  fontSize: "13px",
+  textAlign: "center",
+  color: "#fff",
+  outline: "none",
+  padding: "12px 60px",
+  boxShadow: "2px 5px 10px rgba(0,0,0,.1)",
+  backgroundColor: "rgb(255, 178, 56)",
+  borderRadius: "6px",
+  letterSpacing: "1.5px",
+}
+const Checkout = class extends React.Component {
+  // Initialise Stripe.js with your publishable key.
+  // You can find your key in the Dashboard:
+  // https://dashboard.stripe.com/account/apikeys
+
+  componentDidMount() {
+    this.stripe = window.Stripe("pk_test_E63rEPHarnDbo4p69HHyYw7V00ujoxooMj")
+  }
+  async redirectToCheckout(event, product) {
+    event.preventDefault()
+    const { error } = await this.stripe.redirectToCheckout({
+      items: [{ sku: product, quantity: 1 }],
+      successUrl: `http://localhost:8000/page-2/`,
+      cancelUrl: `http://localhost:8000/`,
+    })
+    if (error) {
+      console.warn("Error:", error)
+    }
+  }
+  render() {
+    const product1 = "sku_GijHEv8twajao0"
+    const product2 = "sku_GijQhjVOrV0STs"
+    return (
+      <div>
+        <button
+          style={buttonStyles}
+          onClick={event => this.redirectToCheckout(event, product1)}
+        >
+          Product1
+        </button>
+        <button
+          style={buttonStyles}
+          onClick={event => this.redirectToCheckout(event, product2)}
+        >
+          Product2
+        </button>
+      </div>
+    )
+  }
+}
+export default Checkout
