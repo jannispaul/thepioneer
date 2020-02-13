0b6a90847db086c58b8cba1a5e0cdbad91b8c1e8
Jannis <jannis@pixel-pink.de>
2020-02-13 17:44:05 +0100

    countdown styles started

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,5 +1,38 @@
 // Countdown code from https://alligator.io/react/countdown-timer-react-hooks/
 import React, { useEffect, useState } from "react"
+import styled from "styled-components"
+
+const StyledH3 = styled.h3`
+  color: white;
+  text-align: center;
+`
+
+const StyledSection = styled.section`
+  padding: 10vh 16px;
+  color: white;
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(9, 9, 121, 1) 35%,
+    rgba(0, 212, 255, 1) 100%
+  );
+`
+const StyledGrid = styled.div`
+  max-width: 500px;
+  display: grid;
+  grid-template-columns: repeat(4, 120px);
+  margin: auto;
+`
+const StyledCell = styled.div``
+
+const TimerCard = styled.div`
+  color: red;
+  background: white;
+  border-radius: 5px;
+  height: 60px;
+  width: 30px;
+`
 
 const Countdown = () => {
   const calculateTimeLeft = () => {
@@ -34,17 +67,20 @@ const Countdown = () => {
     }
 
     timerComponents.push(
-      <span>
-        {timeLeft[interval]} {interval}{" "}
-      </span>
+      <StyledCell>
+        {interval}
+        <TimerCard>{timeLeft[interval]}</TimerCard>
+      </StyledCell>
     )
   })
 
   return (
-    <div>
-      <h2>Timer!</h2>
-      {timerComponents.length ? timerComponents : <span>Es ist soweit!</span>}
-    </div>
+    <StyledSection>
+      <StyledGrid>
+        {timerComponents.length ? timerComponents : <span>Es ist soweit!</span>}
+      </StyledGrid>
+      <StyledH3>Bis zum Start</StyledH3>
+    </StyledSection>
   )
 }
 export default Countdown
