74fccd29bc7690315a0c71ed857679513e835129
Jannis <jannis@pixel-pink.de>
2020-05-05 22:53:11 +0200

    fixed navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,41 +6,51 @@ import PioneerLogo from "./PioneerLogo"
 import PioneerButton from "./PioneerButton"
 
 const StyledNav = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
   position: sticky;
-  padding: 16px;
+  width: 100%;
+  z-index: 100;
+  background: #fff;
   height: auto;
-  & > svg {
-    display: block;
-    max-width: 112px;
-    max-height: 16px;
+  & > div {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 16px;
+    & > svg {
+      display: block;
+      max-width: 112px;
+      max-height: 16px;
+    }
   }
   @media ${device.tablet} {
-    max-width: 1168px;
-    margin: auto;
-    padding: 24px 32px;
-
-    & > svg {
-      max-width: 200px;
-      max-height: 28px;
+    & > div {
+      max-width: 1168px;
+      margin: auto;
+      padding: 20px 32px;
+      & > svg {
+        max-width: 200px;
+        max-height: 28px;
+      }
     }
   }
   @media ${device.laptop} {
-    padding: 32px 32px;
-    & > svg {
-      max-width: 280px;
-      max-height: 38px;
+    & > div {
+      padding: 24px 32px;
+      & > svg {
+        max-width: 280px;
+        max-height: 38px;
+      }
     }
   }
 `
 
 const Nav = props => (
   <StyledNav>
-    <PioneerLogo fill="gradient"></PioneerLogo>
-    <PioneerButton small />
+    <div>
+      <PioneerLogo fill="gradient"></PioneerLogo>
+      <PioneerButton small />
+    </div>
   </StyledNav>
 )
 export default Nav
