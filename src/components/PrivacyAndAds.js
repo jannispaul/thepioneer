53f618ee46e34250c1c05ffeae10394eaa778203
Jannis <jannis@pixel-pink.de>
2020-05-05 10:11:28 +0200

    privacy and ads

--- src/components/PrivacyAndAds.js
+++ src/components/PrivacyAndAds.js
@@ -6,37 +6,70 @@ import lock from "../images/svgs/lock.svg"
 import noAds from "../images/svgs/no-ads.svg"
 
 const StyledSection = styled.section`
-  padding-bottom: 20px;
+  padding-bottom: 30px;
   span {
     text-transform: uppercase;
   }
   img {
     display: block;
     margin-bottom: 12px;
+    height: 60px;
+  }
+  @media ${device.tablet} {
+    padding-bottom: 0;
+    display: flex;
+    flex-wrap: wrap;
   }
 `
 const StyledHalf = styled.div`
   padding: 48px 16px;
   background: #f8f6fd;
+  flex: 1 0 50%;
+
   img {
     max-width: 40px;
+  }
+  @media ${device.tablet} {
+    padding: 80px 32px 200px;
+    & > div {
+      max-width: 500px;
+      float: right;
+    }
+  }
+  @media ${device.laptop} {
+    & > div {
+      margin-right: 50px;
+    }
   }
 `
 const StyledOtherHalf = styled.div`
   padding: 32px 16px;
   background: #fff;
+  flex: 1 0 50%;
+
   img {
     max-width: 60px;
+  }
+  @media ${device.tablet} {
+    padding: 80px 32px;
+    & > div {
+      max-width: 500px;
+    }
+  }
+  @media ${device.laptop} {
+    & > div {
+      margin-left: 50px;
+    }
   }
 `
 
 const StyledButtonContainer = styled.div`
-  margin: auto;
   max-width: calc(100vw - 32px);
-  text-align: center;
-  span {
-    font-size: 14px;
-    color: #8c8c8c;
+  margin: auto;
+
+  @media ${device.tablet} {
+    text-align: center;
+    margin-top: -120px;
   }
 `
 
@@ -49,40 +82,43 @@ const StyledHeadline = styled.h2`
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   padding-right: 4px;
-  font-size: 30px;
+
   margin-bottom: 16px;
 
   @media (${device.tablet}) {
-    font-size: 60px;
     margin-bottom: 21px;
   }
 `
 const PrivacyAndAds = () => (
   <StyledSection>
     <StyledHalf>
-      <img src={lock} alt="" /> <StyledHeadline>Privacy</StyledHeadline>
-      <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-        quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-        arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-        Nullam dict
-      </p>
+      <div>
+        <img src={lock} alt="" /> <StyledHeadline>Privacy</StyledHeadline>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim. Donec pede justo, fringilla vel, aliquet
+          nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
+          venenatis vitae, justo. Nullam dict
+        </p>
+      </div>
     </StyledHalf>
     <StyledOtherHalf>
-      <img src={noAds} alt="" />
-      <StyledHeadline>Werbefreiheit</StyledHeadline>
-      <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-        quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-        arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-        Nullam dict
-      </p>
+      <div>
+        <img src={noAds} alt="" />
+        <StyledHeadline>Werbefreiheit</StyledHeadline>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim. Donec pede justo, fringilla vel, aliquet
+          nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
+          venenatis vitae, justo. Nullam dict
+        </p>
+      </div>
     </StyledOtherHalf>
     <StyledButtonContainer>
       <PioneerButton subline></PioneerButton>
