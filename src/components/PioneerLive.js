dadd15c876e86023bd538cfd4332d27615fd83c6
Jannis <jannis@pixel-pink.de>
2020-05-06 21:49:49 +0200

    fix

--- src/components/PioneerLive.js
+++ src/components/PioneerLive.js
@@ -12,31 +12,36 @@ const StyledSection = styled.section`
   & > div {
     display: flex;
     flex-direction: column;
+    & > p {
+      color: #fff;
+      text-align: center;
+      max-width: 600px;
+      padding: 0 16px;
+      margin: 0 auto 30px;
+    }
   }
   @media ${device.tablet} {
+    padding: 80px 16px 80px 16px;
+
     & > div {
       max-width: 1168px;
       margin: auto;
     }
   }
-
+  /* 
   & > p {
     text-align: center;
     margin: 0 auto 40px auto;
     max-width: 500px;
     padding: 0 16px;
-
-    @media ${device.tablet} {
-    }
-  }
+  } */
 `
 
 const StyledButtonContainer = styled.div`
   max-width: calc(100vw - 32px);
   margin: auto;
-
+  text-align: center;
   @media ${device.tablet} {
-    text-align: center;
   }
 `
 const StyledIframe = styled.iframe`
@@ -58,6 +63,10 @@ const PioneerLive = () => (
       <PioneerHeadline color="#fff" large center>
         Live
       </PioneerHeadline>
+      <p>
+        Live-Journalismus, Video-Streams und exklusive Einblicke in das erste
+        Redaktionsschiff der Welt.
+      </p>
       <StyledIframe
         width="560"
         height="315"
