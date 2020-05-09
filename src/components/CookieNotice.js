475f63e660a1c9b80b10b5a7c54541fab777b65d
Jannis <jannis@pixel-pink.de>
2020-05-09 02:06:04 +0200

    cookienotice

--- src/components/CookieNotice.js
+++ src/components/CookieNotice.js
@@ -2,11 +2,60 @@ import React from "react"
 import CookieConsent from "react-cookie-consent"
 // import { Link } from "gatsby"
 import styled from "styled-components"
+import { device } from "../theme/breakpoints"
 
 const StyledContainer = styled.div`
+  max-width: 100%;
+
+  p {
+    margin-bottom: 20px;
+    @media ${device.tablet} {
+      margin-bottom: 0;
+    }
+  }
+  a,
+  button {
+    display: block;
+    width: 100%;
+    text-align: center;
+    transform: scale(1);
+    transition: all 200ms ease;
+    @media ${device.tablet} {
+      width: auto;
+    }
+  }
+  button {
+    width: calc(100% - 30px);
+    margin: 15px;
+    @media ${device.tablet} {
+      width: auto;
+    }
+  }
+  a:hover,
+  button:hover {
+    transition: all 200ms ease;
+    transform: scale(1.05);
+  }
   & > div {
-    display: flex;
-    align-items: center !important;
+    display: block !important;
+    width: calc(100vw - 20px) !important;
+    border-radius: 10px;
+    margin: 10px;
+    @media ${device.tablet} {
+      max-width: 1168px;
+      left: 0;
+      right: 0;
+      margin: 10px auto;
+      display: flex !important;
+      align-items: flex-end !important;
+    }
+
+    & > div {
+      @media ${device.tablet} {
+        display: flex;
+        align-items: flex-end;
+      }
+    }
   }
 `
 
@@ -15,34 +64,55 @@ const CookieNotice = () => (
     <CookieConsent
       debug={false}
       location="bottom"
-      buttonText="Verstanden"
+      buttonText="Akzeptieren"
       cookieName="WallStreetWeeklyCookieNoticeAccepted"
       style={{
-        position: "relative",
-        background: "#f3eefb",
-        color: "#1f1f1f",
-        boxShadow: "0 0 10px rgba(0,0,0,0.2)",
+        background: "#1f1f1f",
+        color: "#fff",
         zIndex: "200",
-        display: "flex",
+        padding: "20px",
+        // display: "flex",
+        // alignItems: "flex-end",
       }}
       buttonStyle={{
-        background: "#1f1f1f",
-        color: "#e8edfa",
-        fontWeight: "600",
-        fontSize: "18px",
-        padding: "18px",
-        border: "none",
+        background: "#8937f8",
+        color: "#fff",
+        fontWeight: "500",
+        fontSize: "14px",
+        padding: "12px 18px",
+        border: "4px solid #8937F8",
+        borderRadius: "4px",
         cursor: "pointer",
       }}
       expires={365}
     >
-      Diese Website nutzt Cookies um Ihr Erlebnis zu verbessern.{" "}
+      <div>
+        <h3 style={{ color: "#fff", fontSize: "18px", fontWeight: "500" }}>
+          Cookie-Richtlinie
+        </h3>
+        <p style={{ fontFamily: "Noe", color: "#fff", fontSize: "19px" }}>
+          ThePioneer verwendet Cookies, um die Website nutzerfreundlicher,
+          effektiver und sicherer zu machen. Wenn Sie unsere Website nutzen,
+          stimmen Sie der Nutzung und Speicherung von Cookies auf Ihrem Endgerät
+          zu.{" "}
+        </p>
+      </div>
       <a
         href="https://www.thepioneer.de/s/datenschutz"
-        style={{ color: "#1f1f1f" }}
+        style={{
+          border: "2px solid #fff",
+          borderRadius: "4px",
+          color: "#fff",
+          fontSize: "14px",
+          fontWeight: "500",
+          padding: "12px 18px",
+          textDecoration: "none",
+          minWidth: "maxContent !important",
+          flexShrink: "0",
+        }}
         aria-label="Link zum Datenschutz"
       >
-        Mehr erfahren
+        Datenschutzbestimmungen
       </a>
     </CookieConsent>
   </StyledContainer>
