e818bf532453e36c1a9fbcf367ff36d3613d8c99
Jannis <jannis@pixel-pink.de>
2020-07-16 15:39:23 +0200

    added gatsby ssr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,25 +4,25 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
-import React from "react"
-import {
-  ApolloProvider,
-  ApolloClient,
-  HttpLink,
-  InMemoryCache,
-} from "@apollo/client"
+export { wrapRootElement } from "./gatsby-ssr"
+// import React from "react"
+// import {
+//   ApolloProvider,
+//   ApolloClient,
+//   HttpLink,
+//   InMemoryCache,
+// } from "@apollo/client"
 
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "https://www.thepioneer.de/api/graphql",
-    // uri: "https://48p1r2roz4.sse.codesandbox.io",
-    // uri: "https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex",
-    // uri: "https://countries.trevorblades.com/",
-  }),
-})
+// const client = new ApolloClient({
+//   cache: new InMemoryCache(),
+//   link: new HttpLink({
+//     uri: "https://www.thepioneer.de/api/graphql",
+//     // uri: "https://48p1r2roz4.sse.codesandbox.io",
+//     // uri: "https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex",
+//     // uri: "https://countries.trevorblades.com/",
+//   }),
+// })
 
-export const wrapRootElement = ({ element }) => (
-  <ApolloProvider client={client}>{element}</ApolloProvider>
-)
+// export const wrapRootElement = ({ element }) => (
+//   <ApolloProvider client={client}>{element}</ApolloProvider>
+// )
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,27 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+import fetch from "isomorphic-fetch"
+import {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client"
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    fetch,
+    uri: "https://www.thepioneer.de/api/graphql",
+    // uri: "https://48p1r2roz4.sse.codesandbox.io",
+    // uri: "https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex",
+    // uri: "https://countries.trevorblades.com/",
+  }),
+})
+
+export const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>{element}</ApolloProvider>
+)
