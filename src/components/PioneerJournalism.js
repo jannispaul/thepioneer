fac6d85b3003c9fed1633e9132106651b1e5b211
Jannis <jannis@pixel-pink.de>
2021-04-09 00:49:21 +0200

    added benefits and switched cinemagraphs

--- src/components/PioneerJournalism.js
+++ src/components/PioneerJournalism.js
@@ -7,6 +7,11 @@ import PioneerButton from "./PioneerButton"
 import checkmark from "../images/svgs/checkmark.svg"
 import micImage from "../images/pngs/mikrofon.png"
 import PioneerHeadline from "./PioneerHeadline"
+import ship from "../images/svgs/ship.svg"
+import microphone from "../images/svgs/microphone.svg"
+import rocket from "../images/svgs/rocket.svg"
+import pencil from "../images/svgs/pencil.svg"
+import chart from "../images/svgs/chart.svg"
 
 const StyledSection = styled.section`
   background: #f8f6fd;
@@ -25,11 +30,66 @@ const StyledSection = styled.section`
     }
   }
 `
+const Benefits = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  margin: 20px 0 40px;
+  grid-gap: 20px;
+  font-size: 14px;
+
+  @media ${device.mobileL} {
+    font-size: 16px;
+  }
+
+  @media ${device.tablet} {
+    grid-template-columns: 1.1fr 1fr;
+    margin: 20px 0 50px;
+  }
+
+  & > div {
+    display: flex;
+    align-items: center;
+
+    & b {
+      font-weight: 600;
+    }
+    & > div {
+      background: #ebe0fa;
+      border-radius: 50%;
+      width: 50px;
+      height: 50px;
+      margin-right: 20px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-shrink: 0;
+      @media ${device.mobileM} {
+        width: 70px;
+        height: 70px;
+      }
+      & > img {
+        width: 20px;
+        max-height: 22px;
+        @media ${device.mobileM} {
+          width: 28px;
+          max-height: 34px;
+        }
+      }
+    }
+  }
+`
+const BenefitsHeadline = styled.h3`
+  font-family: Poppins;
+  text-transform: uppercase;
+  font-size: 18px;
+  color: #6622c3;
+  font-weight: 600;
+`
 const StyledContainer = styled.div`
   padding: 40px 16px;
   position: relative;
 
-  img {
+  & > img {
     position: absolute;
     right: 0;
     width: 60px;
@@ -76,7 +136,7 @@ const StyledContainer = styled.div`
     & li {
       font-size: 18px;
     }
-    img {
+    & > img {
       margin-right: -32px;
       width: 15vw;
       top: 50px;
@@ -87,7 +147,7 @@ const StyledContainer = styled.div`
     & li {
       font-size: 19px;
     }
-    img {
+    & > img {
       position: absolute;
       right: calc(580px - 50vw);
       width: 15vw;
@@ -96,7 +156,7 @@ const StyledContainer = styled.div`
   }
 `
 
-const PioneerJournalism = props => (
+const PioneerJournalism = (props) => (
   <StyledSection>
     <div>
       <GaborAndMichaelImage></GaborAndMichaelImage>
@@ -105,11 +165,71 @@ const PioneerJournalism = props => (
         <PioneerHeadline>Journalismus</PioneerHeadline>
         <h2>Separating News From Noise</h2>
         <p>
-          Werde Teil unseres internationalen Teams aus leidenschaftlichen
-          Journalisten und Experten und unterstütze eine neue Form des
-          kollaborativen, werbefreien Politik- und Wirtschaftsjournalismus.
+          Werde jetzt Pioneer und unterstütze eine neue Form des kollaborativen,
+          werbefreien Politik- und Wirtschaftsjournalismus.
         </p>
-        <ul>
+        <br />
+        <BenefitsHeadline>Ihre Vorteile als Pioneer</BenefitsHeadline>
+        <Benefits>
+          <div>
+            <div>
+              <img src={ship} alt="" />
+            </div>
+            <span>
+              <b>Steingarts Morning Briefing</b> <br />
+              inklusive aller Spezial-Inhalte
+            </span>
+          </div>
+          <div>
+            <div>
+              <img src={microphone} alt="" />
+            </div>
+            <span>
+              <b>Alle Briefings & Podcasts</b>
+              <br />
+              wie Tech, Hauptstadt oder World Briefing 
+            </span>
+          </div>
+          <div>
+            <div>
+              <img src={ship} alt="" />
+            </div>
+            <span>
+              <b>Live-Journalismus</b>
+              <br />
+              auf dem ersten Medienschiff der Welt
+            </span>
+          </div>
+          <div>
+            <div>
+              <img src={pencil} alt="" />
+            </div>
+            <span>
+              <b>ThePioneer Expert</b>
+              <br />
+              werden und eigene Inhalte publizieren
+            </span>
+          </div>
+          <div>
+            <div>
+              <img src={chart} alt="" />
+            </div>
+            <span>
+              <b>>2.500 Infografiken</b> <br />
+              im digitalen Graphic-Hub
+            </span>
+          </div>
+          <div>
+            <div>
+              <img src={rocket} alt="" />
+            </div>
+            <span>
+              <b>Unterstützung</b> <br />
+              neuer journalistischer Pioneer-Projekte
+            </span>
+          </div>
+        </Benefits>
+        {/* <ul>
           <li>Täglich neue Briefings, Podcasts, Graphics und Live-Inhalte</li>
           <li>
             Exklusiver Live-Journalismus auf dem ersten Redaktionsschiff der
@@ -120,7 +240,7 @@ const PioneerJournalism = props => (
             <PioneerLogo width="100px"></PioneerLogo> Expert
           </li>
           <li>Unterstützung neuer journalistischer Projekte</li>
-        </ul>
+        </ul>*/}
         <PioneerButton subline></PioneerButton>
       </StyledContainer>
     </div>
